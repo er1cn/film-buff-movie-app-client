6626c74e115166315b8c4d0af23e5983db9290c1
Eric Nganga <ericmainaedits@gmail.com>
2021-10-06 05:54:56 -0500

    create components folder add header.js file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-
-import { useEffect } from "react";
 import './App.css';
 
 function App() {
-  useEffect(() => {
-    fetch("http://127.0.0.1:3000/movies")
-      .then((r) => r.json())
-      .then((movies) => console.log(movies));
-  }, []);
 
-  return <h1>Hello from React!</h1>;
+
+  return (
+    <div className="App">
+      <h1>Hello from React!</h1>;
+    </div>
+  );
+  
 }
 
 export default App;
