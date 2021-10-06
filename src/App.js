f390a09170118ee2817212f4e993db7285c88f14
Eric Nganga <ericmainaedits@gmail.com>
2021-10-05 21:24:29 -0500

    first fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+
+import { useEffect } from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    fetch("http://127.0.0.1:3000/movies")
+      .then((r) => r.json())
+      .then((movies) => console.log(movies));
+  }, []);
+
+  return <h1>Hello from React!</h1>;
 }
 
 export default App;
+
+
+
