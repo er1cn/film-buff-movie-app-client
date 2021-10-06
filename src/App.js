05d3bbf676cc4a1a20836b508a17f5f892f4e7d1
Eric Nganga <ericmainaedits@gmail.com>
2021-10-06 06:57:19 -0500

    npm install styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Header from './components/Header/Header';
+
+
+
 
 function App() {
 
 
   return (
     <div className="App">
-      <h1>Hello from React!</h1>;
+      <Header/>
     </div>
   );
   
---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Wrapper, Content } from './Header.styles'
+
+
+
+
+function Header() {
+
+
+    return (
+        <Wrapper>
+          <Content>
+            <h1>Film Buff Movie DB</h1>
+          </Content>  
+        </Wrapper>
+    );
+}
+
+export default Header;
