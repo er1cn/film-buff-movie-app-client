2775bc95d8d31280c0bbe79298119d047af61729
Eric Nganga <ericmainaedits@gmail.com>
2021-10-06 07:33:43 -0500

    add wrapper and content styles

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -1,5 +1,21 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  padding 0 20px;
+  background: #04247C;
+`;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: var(--maxWidth);
+  padding: 20px 0;
+  margin: 0 auto;
+  color: white;
+  font-family: "Bebas Neue", cursive;
+`;
+
+
+  
+
