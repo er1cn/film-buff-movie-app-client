1871e3e68b3d3898b4af2d8dad4842836616dfa6
Eric Nganga <ericmainaedits@gmail.com>
2021-10-09 12:04:03 -0500

    delete home component add API,config,helpers files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import './App.css';
-import Header from './components/Header/Header';
-
-
-
+import React from 'react';
 
 function App() {
-  
-  
-
   return (
     <div className="App">
-      <Header/>
+      Start here.
     </div>
   );
-  
 }
 
 export default App;
-
-
-
