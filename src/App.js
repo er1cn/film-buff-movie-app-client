07a65420909257cd5af01795d5a68247fe930096
Eric Nganga <ericmainaedits@gmail.com>
2021-10-12 13:27:12 -0500

    add Navbar and Login files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { GlobalStyle } from './GlobalStyle';
 import Header from './components/Header';
 import Home from './components/Home';
-
+import Login from './Login';
+import { BASE_URL } from './config';
+import Navbar from './components/Header/Navbar';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch(`${BASE_URL}me`)
+      .then((response) => {
+        if (response.ok) {
+          response.json().then((user) => setUser(user));
+        }
+      });
+  }, []);
+
+  user ? <h2>Welcome, {user.username}!</h2> : <Login onLogin={setUser} />;
+
+  // if (user) {
+  //   return <h2>Welcome, {user.username}!</h2>;
+  // } else {
+  //   return <Login onLogin={setUser} />;
+  // };
+
+
   return (
     <div className="App">
+      <Navbar />
+      <Login />
       <Header />
       <Home />
       <GlobalStyle />
     </div>
-    
   );
 }
 
